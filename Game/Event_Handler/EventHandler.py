16b9c258e8752d0376c3490e77b4764608bf0264
Razvan Bichiu <irazinfi@gmail.com>
2024-02-17 01:54:52 +0200

    Character attack and health and monster attack done(forgot monster image)

--- Game/Event_Handler/EventHandler.py
+++ Game/Event_Handler/EventHandler.py
@@ -1,21 +1,23 @@
 import pygame as pg
 from constants import *
-from Game.Interface.Monster_and_player_classes import Player
+from Game.Interface.Monster_and_player_classes import *
 class EventHandler:
     pg.init()
     def __init__(self):
         pass
 
-    def Event_L_Key(self,screen):
+    '''Event for liking the live on tiktok, creates a text above the player's heads'''
+    def Event_L_Key(self,screen,monster,player):
         font = pg.font.Font(pg.font.get_default_font(), 16)
         text = font.render("Player Attack!", True,  BLACK, WHITE)
         textrect= text.get_rect()
         textrect.center = (700,300)
         screen.blit(text, textrect)
+        monster.take_damage(player.attack)
 
-
-    def monster_attack(self, player, damage):
-        player.take_damage(damage)
+    '''Event for the timerevent being done and attacking the player'''
+    def monster_attack(self, player, monster):
+        player.take_damage(monster.attack)
 
 
 
--- Game/Interface/GameWindow.py
+++ Game/Interface/GameWindow.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from Game.Event_Handler.EventHandler import EventHandler
 from constants import *
-from Game.Interface.Monster_and_player_classes import Player
+from Game.Interface.Monster_and_player_classes import *
 class GameWindow:
     pg.init()
     def __init__(self):
@@ -10,12 +10,35 @@ class GameWindow:
         self.width = 800
         self.height = 600
         self.screen = pg.display.set_mode((self.width,self.height))
+
         player_image = pg.image.load('images/player.png')
         self.playerVisual = pg.transform.smoothscale(player_image,(200,200))
-        self.player=Player(1,50,100, self.playerVisual)
+        self.player=Player(1,50,100, self.playerVisual,1)
+
         self.Monster_attack_event=pg.USEREVENT+1
         pg.time.set_timer(self.Monster_attack_event,10000)
+        monster1_image = pg.image.load('images/Monster1.png')
+        self.monster1Visual = pg.transform.smoothscale(monster1_image,(150,150))
+        self.monster=Monster1(5,100,100, self.monster1Visual)
 
+    def draw_stat_bar(self,player):
+        '''This function draws the player's health and attack on the screen. It takes in the player object.'''
+        font = pg.font.Font(None, 36)
+        health_text = font.render(f'Health: {player.health_current}/{player.health_max}', True, BLACK)
+        attack_text = font.render(f'Attack: {player.attack}', True, BLACK)
+        level_text = font.render(f'Level: {player.level}', True, BLACK)
+        self.screen.blit(level_text, (10, 460))
+        self.screen.blit(health_text, (10, 500))
+        self.screen.blit(attack_text, (10, 540))
+
+    def draw_monster_health(self, screen, monster):
+        '''This function draws the monster health on the screen. It takes in the screen and the monster object.'''
+        self.draw_health_rectangle(screen, 125, 75, monster.health_current)
+    def draw_monster(self, screen, monster):
+        '''This function draws the monster on the screen. It takes in the screen and the monster object.'''
+        screen.blit(monster.image, (100, 100))
+
+    '''This function draws the health bar on the screen. It takes in the screen, the x and y coordinates of the health bar,'''
     def draw_health_rectangle(self, screen, x, y, health):
         if health < 0:
             health = 0
@@ -27,19 +50,26 @@ class GameWindow:
         pg.draw.rect(screen, RED, fill_rect)
         pg.draw.rect(screen, BLACK, outline_rect, 2)
 
+    '''This function runs the game and updates the screen. It also checks for events 
+    such as the monster attack event and the quit event. It also checks for the like press event.'''
     def run_game(self):
+        pg.mixer_music.load('images/soundtrack.mp3')
+        pg.mixer_music.play(-1)
         while self.run:
             pg.time.delay(100)
             self.screen.fill((255,255,255))
             self.draw_health_rectangle(self.screen, 650, 500, self.player.health_current)
+            self.draw_monster(self.screen, self.monster)
+            self.draw_monster_health(self.screen, self.monster)
+            self.draw_stat_bar(self.player)
             key = pg.key.get_pressed()
             if key[pg.K_l]:
-                self.events.Event_L_Key(self.screen)
+                self.events.Event_L_Key(self.screen,self.monster,self.player)
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.run = False
                     pg.quit()
                 elif event.type == self.Monster_attack_event:
-                    self.events.monster_attack(self.player, 5)
+                    self.events.monster_attack(self.player, self.monster)
             self.screen.blit(self.player.image,(600,300))
             pg.display.update()
