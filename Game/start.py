d160119b5c7f9443daf2922cdeabe342c599f86d
Razvan Bichiu <irazinfi@gmail.com>
2024-02-19 17:23:05 +0200

    messing with tiktoklive

--- Game/start.py
+++ Game/start.py
@@ -1,6 +1,14 @@
+import Tiktok_connector.TiktokConnector
 from Game.Interface.GameWindow import GameWindow
-
+from threading import Thread
+import asyncio
+giftlist=[]
 def main():
     game = GameWindow()
+    thread=Thread(target=Tiktok_connector.TiktokConnector.runTiktokClient, args=(giftlist,))
+    thread.daemon = True
+    thread.start()
+    print(giftlist,"3")
+    print(id(giftlist),"4")
     game.run_game()
 main()
--- Tiktok_connector/TiktokConnector.py
+++ Tiktok_connector/TiktokConnector.py
@@ -1,9 +1,9 @@
 from TikTokLive import TikTokLiveClient
-from TikTokLive.types.events import CommentEvent, ConnectEvent
+from TikTokLive.types.events import *
 
+ListofGifts = []
 # Instantiate the client with the user's username
-client: TikTokLiveClient = TikTokLiveClient(unique_id="@isaackogz")
-
+client: TikTokLiveClient = TikTokLiveClient(unique_id="@wakbogel_2")
 
 # Define how you want to handle specific events via decorator
 @client.on("connect")
@@ -18,23 +18,26 @@ async def on_comment(event: CommentEvent):
 
 @client.on("like")
 async def on_like(event: LikeEvent):
-    print(f"@{event.user.unique_id} liked the stream!")
+    ListofGifts.append(event)
 
 @client.on("gift")
 async def on_gift(event: GiftEvent):
-    # If it's type 1 and the streak is over
-    if event.gift.info.type == 1:
-        if event.gift.is_repeating == 1:
-            print(f"{event.user.unique_id} sent {event.gift.count}x \"{event.gift.info.name}\"")
+    # Streakable gift & streak is over
+    if event.gift.streakable and not event.gift.streaking:
+        ListofGifts.append(event)
 
-    # It's not type 1, which means it can't have a streak & is automatically over
-    elif event.gift.info.type != 1:
-        print(f"{event.user.unique_id} sent \"{event.gift.info.name}\"")
+    # Non-streakable gift
+    elif not event.gift.streakable:
+        ListofGifts.append(event)
 
 # Define handling an event via a "callback"
 client.add_listener("comment", on_comment)
-
-if __name__ == '__main__':
-    # Run the client and block the main thread
-    # await client.start() to run non-blocking
+def runTiktokClient(giftlist):
+    global ListOfGifts
+    ListOfGifts = giftlist
+    ListOfGifts.append("lmao")
+    print(id(giftlist),"1")
+    print(id(ListofGifts),"2")
     client.run()
+
+
